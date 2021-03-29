ad73a8ef73458451dfcfc3f5e5bafa197b9f1e81
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2021-03-29 14:23:03 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Error404Component } from './core/error404/error404.component';
+import { HomeComponent } from './core/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './core/material.module';
+
+import { HomeComponent } from './core/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Error404Component } from './core/error404/error404.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
